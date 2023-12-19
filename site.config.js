08dc140fde57937da6f7e4da4e009173389749b4
busybean3 <dbwo4011@gmail.com>
2023-12-19 21:53:34 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -11,10 +11,10 @@ const CONFIG = {
     instagram: "",
   },
   projects: [
-    /*{
+    {
       name: `busybean3`,
       href: "https://github.com/morethanmin/morethan-log",
-    },*/
+    },
   ],
   // blog setting (required)
   blog: {
