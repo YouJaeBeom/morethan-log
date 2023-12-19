ee2068be62c2ba27385c57cd9e8d0e518785b959
busybean3 <dbwo4011@gmail.com>
2023-12-19 21:51:27 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -5,9 +5,9 @@ const CONFIG = {
     image: "/avatar.jpg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
+    email: "dbwo4011@gmail.com",
     linkedin: "",
-    github: "busybean3",
+    github: "YouJaeBeom",
     instagram: "",
   },
   projects: [
